f539d924e902a2214fe99458f4b5fb0c40163c35
Vitor Miguel <vitorlb2125@gmail.com>
2026-07-13 17:33:44 -0300

    fix: update WhatsApp CTA on current theme

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const BRAND_NAME = "W3 Tráfego Pago"
-export const CTA_HREF = "https://api.whatsapp.com/send/?phone=5568992523482&text&type=phone_number&app_absent=0"
+export const CTA_HREF = "https://api.whatsapp.com/send/?phone=5568992523482&text=Olá!+Vim+pelo+site+da+W3+Tráfego+Pago+e+gostaria+de+falar+com+um+especialista&type=phone_number&app_absent=0"
 export const CTA_LABEL = "Falar com Especialista"
